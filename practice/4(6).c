afc262dc6e5d622154950231623d01e28a8a934e
Jigarvekariya <148442983+Jigarvekariya@users.noreply.github.com>
2024-01-01 16:46:44 +0530

    Add files via upload

--- practice/4(6).c
+++ practice/4(6).c
@@ -1,7 +1,16 @@
 #include<stdio.h>
-
-int main()
+int main(int argc, char const *argv[])
 {
+    int i,c,a=0,b=0,d;
+    printf("enter Fibonacci series no");
+    scanf("%d",&d);
+    for (i = 0; i <d ; i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+        printf("Fibonacci series %d\n",c);
+    }
 
 
     return 0;
---
+++ practice/4(7).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int number,a,b;
+    printf("Enter Number");
+    scanf("%d",&number);
+
+    while(number !=0)
+    {
+        a=number%10;
+        b=b*10+a;
+        number/=10;
+
+    }
+        printf("%d",b);
+
+    return 0;
+}
