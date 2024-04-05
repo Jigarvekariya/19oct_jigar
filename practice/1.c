f3a23a4a6fba59b5ead4b0aa5d6951df85d51196
Jigarvekariya <148442983+Jigarvekariya@users.noreply.github.com>
2024-04-05 15:31:44 +0530

    Delete practice directory

--- practice/1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("name:-Jigar\n");
-    printf("date of Birth date:-10/2/2002\n");
-    printf("age:-21\n");
-    printf("address:-satelight chowk,morbi road rajkot,360003");
-}
